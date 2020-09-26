b3488d0cbce4de5060afe7c43e64d8f79c0490aa
IlyaBaitov <ameba.ilesha@yandex.ru>
2020-09-26 13:29:06 +0500

    Теперь можно добавлять только кнопку и записи в БД с другим 'type'

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Блог</title>
+</head>
+<body>
+    <form action="./Articles/article.php" method="post">
+        <input type="submit" name="Car" id="Car" value="Машинки">
+        <input type="submit" name="Hunt" id="Hunt" value="Охота">
+        <input type="submit" name="Food" id="Food" value="Еда">
+    </form>
+</body>
+</html>
